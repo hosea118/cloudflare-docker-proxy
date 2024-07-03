d65577a69f307a29182dec9bf6645a75a33f939b
hosea118 <think.improve@gmail.com>
2024-07-03 18:14:47 +0800

    Update index.js: for thinkim.cn

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.thinkim.cn": dockerHub,
+  "quay.thinkim.cn": "https://quay.io",
+  "gcr.thinkim.cn": "https://gcr.io",
+  "k8s-gcr.thinkim.cn": "https://k8s.gcr.io",
+  "k8s.thinkim.cn": "https://registry.k8s.io",
+  "ghcr.thinkim.cn": "https://ghcr.io",
+  "cloudsmith.thinkim.cn": "https://docker.cloudsmith.io",
+  "ecr.thinkim.cn": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.thinkim.cn": dockerHub,
 };
 
 function routeByHosts(host) {
